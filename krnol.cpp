a136c6263ffdd0705ed7cc4f88f105cc16586623
ShinpiMeiro <max.rozoff@gmail.com>
2023-09-21 11:20:54 +0300

    added board

--- krnol.cpp
+++ krnol.cpp
@@ -29,16 +29,26 @@ char hod_igroka(int* player_place_x_t, int* player_place_y_t) {
 }
 
 
-void drawing_desk(int desk_t){
-    ;
-}
+void drawing_desk(int ( desk_t )[3][3]) {
 
+
+    cout << "╔═══╤═══╤═══╗" << '\n';
+    cout << "║ " << desk_t[0][0] << " │ " << desk_t[1][0] << " │ " << desk_t[2][0] << " ║" << '\n';
+    cout << "╟───┼───┼───╢" << '\n';
+    cout << "║ " << desk_t[1][1] << " │ " << desk_t[1][1] << " │ " << desk_t[2][1] << " ║" << '\n';
+    cout << "╟───┼───┼───╢" << '\n';
+    cout << "║ " << desk_t[1][2] << " │ " << desk_t[1][2] << " │ " << desk_t[2][2] << " ║" << '\n';
+    cout << "╚═══╧═══╧═══╝" << '\n';
+
+}
 
 int main() {
     int player_place_x,player_place_y;
     int desk[3][3];
+    desk[2][2] = 0;
     drawing_desk(desk);
     hod_igroka(&player_place_x, &player_place_y);
+
 
 
     cout << player_place_x << player_place_y;
